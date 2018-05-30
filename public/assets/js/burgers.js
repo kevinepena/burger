db6149a72df5e54aec046148ea2d02584b3f7aa7
Kevin Pena <kevinpena160@gmail.com>
2018-05-30 11:04:42 -0500

    working

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,27 +1,30 @@
-
-
-
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
     $(".change-devoured").on("click", function(event) {
+
       var id = $(this).data("id");
       var newDevoured = $(this).data("newdevoured");
+
+      if (newDevoured) {
+        var newDevouredState = {
+          devoured: newDevoured
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newDevouredState
+        }).then(
+          function() {
+            console.log("changed devoured to", newDevoured);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      } else {
+        return;
+      }
   
-      var newDevouredState = {
-        devoured: newDevoured
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevouredState
-      }).then(
-        function() {
-          console.log("changed devoured to", newDevoured);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
     });
   
     $(".create-form").on("submit", function(event) {
